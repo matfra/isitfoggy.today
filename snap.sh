2608ef603954d3b0cf79c50c3a2f9d4d1b2fac64
Mathieu <supermattt@gmail.com>
2017-10-18 07:52:09 -0700

    Added percentage base matching

--- snap.sh
+++ snap.sh
@@ -6,6 +6,7 @@ PIC_DIR="/var/photos"
 PIC_DIR_SIZE=20000000
 #PIC_DIR_SIZE=100001
 TMP_PIC_PATH="/tmp/picture.jpg"
+DEFAULT_PERCENT_MATCH=92
 
 function is_dir_bigger_than() {
 	dir=$1
@@ -25,6 +26,8 @@ function purge_oldest_day_in_dir() {
 function is_picture_close_enough_to_previous() {
 	new_pic=$1
 	prev_pic=$(readlink -f $2)
+	match_percentage=$3
+	sq_match_percentage=$(( $match_percentage * $match_percentage ))
 	if [[ ! -f $new_pic ]] ; then
 		echo "$new_pic doesn't exist, probably a capture problem, will retry"
 		return 1
@@ -35,11 +38,11 @@ function is_picture_close_enough_to_previous() {
 	fi
 	new_pic_size=$(ls -s $new_pic |cut -d ' ' -f 1)
 	prev_pic_size=$(ls -s $prev_pic |cut -d ' ' -f 1)
-	sqrt_diff_size=$(( (new_pic_size - prev_pic_size) * (new_pic_size - prev_pic_size) ))
+	sq_pcent_diff_size=$(( ( 100 * new_pic_size / (prev_pic_size + 1 )) * ( 100 * new_pic_size / (prev_pic_size + 1)) ))
 	echo "Old pic: $prev_pic_size"
 	echo "New pic: $new_pic_size"
-	echo "Delta: $sqrt_diff_size"
-	[[ $sqrt_diff_size -lt 100 ]] && echo "The two pictures also match" && return 0 || return 1
+	echo "Match: $sq_pcent_diff_size / 10000. Objective: $sq_match_percentage"
+	[[ $sq_pcent_diff_size -gt $sq_match_percentage ]] && echo "The two pictures match" && return 0 || return 1
 }
 
 function capture_to_file() {
@@ -67,14 +70,12 @@ while true; do
 	mkdir -p $PIC_DIR/$cur_date
 	capture_to_file "$TMP_PIC_PATH"
 	retry=1
-	while ! is_picture_close_enough_to_previous $TMP_PIC_PATH $PIC_DIR/latest.jpg ;do
+	percent_match=$DEFAULT_PERCENT_MATCH
+	while ! is_picture_close_enough_to_previous $TMP_PIC_PATH $PIC_DIR/latest.jpg $percent_match;do
 		echo "Capturing (try: $retry)"
 		capture_to_file "$TMP_PIC_PATH"
 		retry=$(( $retry + 1))
-		if [[ $retry -gt 30 ]] ; then
-			echo "Giving up trying to find a good match"
-			break
-		fi
+		percent_match=$(( $percent_match - ( $retry / 30 ) ))
 	done
 	new_file_path=$PIC_DIR/$cur_date/$cur_time.jpg
 	cp $TMP_PIC_PATH $PIC_DIR/$cur_date/$cur_time.jpg
