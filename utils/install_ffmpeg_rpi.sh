4fbbeb4e9da79e6424c28ba2feb157fca7cd93f8
Mathieu <supermattt@gmail.com>
2018-01-18 21:18:59 -0800

    Better raspberry pi ffmpeg compilation

---
+++ utils/install_ffmpeg_rpi.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -ex
+FFMPEG_SRC_DIR=/tmp/FFmpeg
+
+case $(uname -m) in 
+armv6l)
+    ARCH=armel
+    ;;
+
+armv7l)
+    ARCH=armhf
+    ;;
+*)
+    [[ -z $ARCH ]] && echo "Not sure about your CPU architecture.
+    Please run again and export the ARCH variable."
+    ;;
+esac
+
+sudo apt-get update
+sudo \
+apt-get -y install \
+git \
+autoconf \
+automake \
+build-essential \
+libass-dev \
+libfreetype6-dev \
+libsdl2-dev \
+libtheora-dev \
+libtool \
+libva-dev \
+libvdpau-dev \
+libvorbis-dev \
+libxcb1-dev \
+libxcb-shm0-dev \
+libxcb-xfixes0-dev \
+pkg-config \
+texinfo \
+wget \
+zlib1g-dev \
+libx264-dev
+
+mkdir -p $FFMPEG_SRC_DIR
+cd $FFMPEG_SRC_DIR
+
+if ! [[ -d $FFMPEG_SRC_DIR/.git ]] ; then
+    git clone https://github.com/FFmpeg/FFmpeg.git --depth=1 --branch master --single-branch .
+else
+    git status
+    git checkout master
+    git pull origin master
+    make clean
+fi
+
+./configure --arch=$ARCH --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree --enable-mmal 
+make -j $(nproc --all)
+sudo make install 
