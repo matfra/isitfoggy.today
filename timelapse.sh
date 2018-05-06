96aa42ce5233503bb680808e83e98a88dc5a40c1
Mathieu <supermattt@gmail.com>
2018-05-06 10:23:37 -0700

    Archiving after a timelapse has been succesfully created

--- timelapse.sh
+++ timelapse.sh
@@ -1,21 +1,6 @@
 #!/bin/bash -ex
-CONFIG_FILE=/etc/isitfoggy.conf
-source $CONFIG_FILE
 
-function is_dir_bigger_than() {
-	dir=$1
-	limit=$2
-	dir_size=$(du -s $dir |awk '{print $1}')
-	limit=$2
-	[[ $dir_size -gt $limit ]] && return 0 || return 1
-}
-
-function purge_oldest_day_in_dir() {
-	dir=$1
-	dir_to_purge=$(find $dir -type d -regextype sed -regex ".*/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" |sort -rn |tail -1)
-	echo "Purging directory: $dir_to_purge"
-	[[ -z $dir_to_purge ]] || rm -rf $dir_to_purge
-}
+source $(dirname $0)/common.sh
 
 function send_to_ftp() {
     ftp -n $FTP_HOST <<END_SCRIPT
@@ -40,14 +25,13 @@ if [[ "$today_dir" == "$yesterday_dir" ]] ; then
 	exit 1
 fi
 
-while is_dir_bigger_than "$PIC_DIR" $PIC_DIR_SIZE ; do
-	purge_oldest_day_in_dir "$PIC_DIR"
-done
+pre_flight_checks
 
 for i in $(find $yesterday_dir -type f -name '*.jpg' |sort -n) ; do echo "file '$i'" ; done > $PIC_DIR/timelapse.txt
 
 nice -n 10 /usr/local/bin/ffmpeg -f concat -safe 0 -i $PIC_DIR/timelapse.txt -c:v h264_omx -b:v 8M -vf fps=24 $yesterday_dir/timelapse.mp4 && \
-ln -sf $yesterday_dir/timelapse.mp4 $PIC_DIR/latest.mp4
+ln -sf $yesterday_dir/timelapse.mp4 $PIC_DIR/latest.mp4 && \
+archive_dir $yesterday_dir 30
 
 [[ -z $FTP_HOST ]] || send_to_ftp
 
