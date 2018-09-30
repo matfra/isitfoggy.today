22d4de3fd652b3d9f5364edf8c3aa0e32eb49fb5
Nathan Handler <nhandler@users.noreply.github.com>
2018-09-30 12:57:41 -0700

    Specify -n in nice command in install_ffmpeg_rpi.sh
    Currently, running install_ffmpeg_rpi.sh will fail with "nice: '10': No such file or directory". This is because you need -n before the 10.

--- utils/install_ffmpeg_rpi.sh
+++ utils/install_ffmpeg_rpi.sh
@@ -53,6 +53,6 @@ else
     make clean
 fi
 
-nice 10 ./configure --arch=$ARCH --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree --enable-mmal 
-nice 10 make -j $(nproc --all)
+nice -n 10 ./configure --arch=$ARCH --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-nonfree --enable-mmal 
+nice -n 10 make -j $(nproc --all)
 sudo make install 
