110ec2cf3da6b26db1bb0b541c7d2e7f9561d1b9
Mathieu Frappier <mathieu.frappier@gmail.com>
2021-11-18 23:02:11 -0800

    Added custom daylight generator

--- daylight.sh
+++ daylight.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+
 function get_yesterday_dir() {
 	cur_date=$(date +%Y-%m-%d)
 	find $PIC_DIR/ -type d -regextype sed -regex ".*/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" |sort -n |tail -2 |head -1
@@ -9,6 +10,7 @@ function usage() {
 	echo "usage: $0 [ -d | --dir directory to look into defaults to yesterday] [ -v | --verbose ] | [ -h | --help ]"
 }
 
+# Parameters used to select the zone of the sky to average the color from
 crop_zone="320x200+80+150"
 gravity="North"
 force=0
@@ -18,6 +20,9 @@ while [[ $# -gt 0 ]]; do
 		-v | --verbose )	set -x
 					;;
 		-f | --force )		force=1
+					;;
+		-n | --numdays )	shift
+					numdays=$1
 					;;
 		-d | --dir )		shift
 					working_dir=$1
@@ -37,6 +42,7 @@ while [[ $# -gt 0 ]]; do
 	shift
 done
 
+# Load the common isitfoggy tooling library
 source $(dirname $(readlink -f $0))/common.sh 
 pre_flight_checks
 
@@ -84,12 +90,67 @@ function generate_yearview() {
 		
 }
 
+function generate_lastndayview() {
+	DEFAULT_COUNT=365
+	requested_day_count="${1:-$DEFAULT_COUNT}"
+
+	days=$(find $PIC_DIR -name daylight.png | sort -n |tail -n $requested_day_count)
+	day_count=$(echo "$days"|wc -l)
+	[[ ! $day_count == $requested_day_count ]] && echo "There is only data for $day_count days but $requested_day_count were requested" && exit 1 
+	# Generate the png	
+	file_prefix="daylight_last_${day_count}_days"
+	convert +append $(echo $days) $PIC_DIR/$file_prefix.png
+
+	# Generate the html
+	HTML_DIR=$(dirname $(readlink -f $0))/html
+	HTML_FILE=$TMP_DIR/$file_prefix.html
+	echo "<html><head><title>Daylight for the last $day_count days</title><link rel=\"stylesheet\" href=\"daylight.css\"></head><body>" > $HTML_FILE
+	echo "<img src=\"photos/$file_prefix.png\" usemap=\"#daylight\" width=\"${day_count}\" height=\"1440\">">> $HTML_FILE
+	echo "<map name=\"daylight\">" >> $HTML_FILE
+	x=0
+	for image_dir in $days ; do
+		image_of_the_day="/$(echo $image_dir |cut -d '/' -f5-)"
+		dirlink="$(echo $image_of_the_day |cut -d '/' -f1-3)/"
+		altname="$(echo $dirlink | cut -d '/' -f3)"
+		echo "  <area shape=\"rect\" coords=\"$x,0,$((x+1)),1439\" alt=\"$altname\" href=\"$dirlink\">" >> $HTML_FILE
+		x="$((x+1))"
+	done
+	echo '</map><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script><script src="lib/jquery.rwdImageMaps.min.js"></script>' >> $HTML_FILE
+	echo "<script>\$(document).ready(function(e) { \$('img[usemap]').rwdImageMaps();});</script></div></body></html>" >> $HTML_FILE
+	mv $HTML_FILE $HTML_DIR/
+		
+}
+
+
+#TO-DO for V2
+#Create a list of all the years with daylight file
+#Order them from the oldest to the newest
+#Count them by year
+#For every year, if there is a png of the right width, skip
+#If not, generate a png with all the days of the given year
+#Generate a clickable html page
+#Grab the last 365 days and generate a png and html of that
+
+#If there is already a png for the 
+#Figure out the year we are are in
+#Generate the png for the last 365 days
+#For every year, if it's the current year
+#Count how many days we have this year
+#If the 
+#If there is no existing png for the current year, create one
+#Generate html with a list box for all the years pngs
+
 
 #main
+if [[ ! -z $numdays ]] ; then
+	generate_lastndayview $numdays
+	exit 0
+fi
 
 if [[ $force == 1 ]] || ! test -f $working_dir/daylight.png ; then
 	generate_daylight $working_dir
 	[[ $? == 0 ]] || exit 1
 fi
 set +e
+
 generate_yearview
