ea1e8fc63a03242ab538a01b67fe2789e2e482dd
Mathieu Frappier <mathieu.frappier@gmail.com>
2021-11-23 16:09:50 -0800

    Cleaned up daylight script

--- daylight.sh
+++ daylight.sh
@@ -3,18 +3,32 @@ set -euo pipefail
 
 # TODO: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 
-function get_last_day() {
-	basename $(find $PIC_DIR/ -type d -regextype sed -regex ".*/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" |sort -n |tail -2 |head -1) 
-}
-
-function usage() {
-	echo "usage: $0 [ -d | --dir directory to look into defaults to yesterday] [ -v | --verbose ] | [ -h | --help ]"
-}
-
-# Parameters used to select the zone of the sky to average the color from
+# Defaults args
 crop_zone="320x200+80+150"
 gravity="North"
+
+function usage() {
+	echo "Usage: $0 [OPTIONS]... REQUIRED_FLAG ARGUMENT
+Generate an HTML browser for daily pictures allowing the user to find a single day or hour in years worth of data.
+
+Optional flags:
+[ -f | --force ] 	Force overwrite the existing daylight data
+[ -w | --html_only ] 	Generate only the HTML of a given month, not the png
+[ -v | --verbose ] 	Enable debug output via bash -x option
+[ -c | --crop_zone ]	Imagemagick crop zone to get the average color of the sky. Defaults to $crop_zone
+[ -g | --gravity ]	Imagemagick side of the picture to run the position the crop zone from. Defaults to $gravity
+
+Required flags:
+-h | --help 		This help
+-b | --browser_only 	Generate the HTML browser only
+-m | --month 		Generate the band for a given month. Example: -m 2020-11
+-r | --range		Generate the bands for all the months in the range. Example : -r 2020-01,2021-08
+"
+}
+
 force=0
+browser_only=0
+html_only=0
 
 [[ $# -lt 1 ]] && usage && exit 1
 
@@ -26,29 +40,22 @@ while [[ $# -gt 0 ]]; do
 					;;
 		-w | --html_only )	html_only=1
 					;;
-		-n | --numdays )	shift
-					numdays=$1
-					;;
-		-y | --year )		shift
-					year="$1"
-					;;
-		-m | --month )		shift
-					month=$1
-					;;
-		--from )		shift
-					month_from=$1
-					;;
-		--to )			shift
-					month_to=$1
-					;;
-		-d | --day )		shift
-					day=$1
+		-b | --browser_only )	browser_only=1
 					;;
 		-c | --crop )		shift
 					crop_zone=$1
 					;;
 		-g | --gravity )	shift
 					gravity=$1
+					;;
+		-m | --month )		shift
+					month=$1
+					;;
+		-d | --day )		shift
+					day=$1
+					;;
+		-r | --range )		shift
+					range=$1
 					;;
 		-h | --help )           usage
 					exit
@@ -63,8 +70,31 @@ done
 # Load the common isitfoggy tooling library
 source $(dirname $(readlink -f $0))/common.sh 
 pre_flight_checks
+HTML_DIR=$(dirname $(readlink -f $0))/html
+
+
+##################
+# Helper functions
+
+function validate_iso_month() {
+	echo "$1" |grep -q -E '^[0-9]{4}\-[0-1][0-9]$'
+	return $?
+}
+
+function sanitize_iso_date() {
+	date -d "$1" "+%Y-%m-%d"
+}
+
+function fatal () {
+	$message="FATAL $1"
+	exit 1
+}
+
+################
+# Generate bands
 
 function generate_day_band() {
+	# Historical: Takes workdir as an argument, typically $PIC_DIR/1986-09-30
 	WORKDIR=$1
 	[[ ! -d $WORKDIR ]] && echo "WARN $WORKDIR does not exist: Skipping it" && return 0
 	tmpfile=$TMP_DIR/tmp_daylight_$$.txt
@@ -87,103 +117,6 @@ function generate_day_band() {
 	done
 	convert $tmpfile $WORKDIR/daylight.png
 	rm $tmpfile
-}
-	
-function generate_yearview() {
-	days=$(find $PIC_DIR -name daylight.png | sort -n)
-	# Generate the png	
-	convert +append $(echo $days) $PIC_DIR/light.png
-
-	# Generate the html
-	HTML_DIR=$(dirname $(readlink -f $0))/html
-	HTML_FILE=$HTML_DIR/daylight_browser.html
-	number_of_days=$(echo "$days"|wc -l)
-	relative_width=$(echo $number_of_days |perl -lne 'print 100/$_')
-	echo "<html><head><title>Daylight for the last $number_of_days days</title></head><body style=\"margin: 0px; background: #0e0e0e;\"><div align=\"center\">" > $HTML_FILE
-	for image_dir in $days ; do
-		image_of_the_day="/$(echo $image_dir |cut -d '/' -f5-)"
-		dirlink="$(echo $image_of_the_day |cut -d '/' -f1-3)/"
-		echo "<a style=\"margin-right: -4px;\" href=\"$dirlink\"><img src=\"$image_of_the_day\" width=\"${relative_width}%\" height=\"100%\"></a>" >> $HTML_FILE
-	done
-	echo "</div></body></html>" >> $HTML_FILE
-		
-}
-
-function generate_lastndayview() {
-	DEFAULT_COUNT=365
-	requested_day_count="${1:-$DEFAULT_COUNT}"
-
-	days=$(find $PIC_DIR -name daylight.png | sort -n |tail -n $requested_day_count)
-	day_count=$(echo "$days"|wc -l)
-	[[ ! $day_count == $requested_day_count ]] && echo "There is only data for $day_count days but $requested_day_count were requested" && exit 1 
-	# Generate the png	
-	file_prefix="daylight_last_${day_count}_days"
-	convert +append $(echo $days) $PIC_DIR/$file_prefix.png
-
-	# Generate the html
-	HTML_DIR=$(dirname $(readlink -f $0))/html
-	HTML_FILE=$TMP_DIR/$file_prefix.html
-	echo '<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>isitfoggy daylight browser</title>
-    <link rel="stylesheet" href="daylight.css">
-  </head>
-    <body>
-      <div class="top">
-        <div class="bands">' >> $HTML_FILE
-	echo "<img src=\"photos/$file_prefix.png\" usemap=\"#daylight\" width=\"${day_count}\" height=\"1440\">">> $HTML_FILE
-	echo "<map name=\"daylight\">" >> $HTML_FILE
-	x=0
-	for image_dir in $days ; do
-		image_of_the_day="/$(echo $image_dir |cut -d '/' -f5-)"
-		dirlink="$(echo $image_of_the_day |cut -d '/' -f1-3)/"
-		altname="$(echo $dirlink | cut -d '/' -f3)"
-		echo "  <area shape=\"rect\" coords=\"$x,0,$((x+1)),1439\" alt=\"$altname\" href=\"$dirlink\">" >> $HTML_FILE
-		x="$((x+1))"
-	done
-	echo '</map>     
-      </div>
-      
-      <div class="right">
-          <div class="timeboxeven">12 AM</div>
-          <div class="timeboxodd">1 AM</div>
-  <div class="timeboxeven">2 AM</div>
-  <div class="timeboxodd">3 AM</div>
-  <div class="timeboxeven">4 AM</div>
-  <div class="timeboxodd">5 AM</div>
-  <div class="timeboxeven">6 AM</div>
-  <div class="timeboxodd">7 AM</div>
-  <div class="timeboxeven">8 AM</div>
-  <div class="timeboxodd">9 AM</div>
-  <div class="timeboxeven">10 AM</div>
-  <div class="timeboxodd">11 AM</div>
-  <div class="timeboxeven">12 PM</div>
-  <div class="timeboxodd">1 PM</div>
-  <div class="timeboxeven">2 PM</div>
-  <div class="timeboxodd">3 PM</div>
-  <div class="timeboxeven">4 PM</div>
-  <div class="timeboxodd">5 PM</div>
-  <div class="timeboxeven">6 PM</div>
-  <div class="timeboxodd">7 PM</div>
-  <div class="timeboxeven">8 PM</div>
-  <div class="timeboxodd">9 PM</div>
-  <div class="timeboxeven">10 PM</div>
-  <div class="timeboxodd">11 PM</div>
-  </div>
-  </div>
-  <div class="bottom">Months and years here</div>
-  <script src="lib/jquery.min.js"></script>
-  <script src="lib/jquery.rwdImageMaps.min.js"></script>
-
-    <script>
-    $(document).ready(function(e) { $("img[usemap]").rwdImageMaps();});
-    </script>
-    </body>
-</html>' >> $HTML_FILE
-	mv $HTML_FILE $HTML_DIR/
-		
 }
 
 function generate_black_band() {
@@ -208,6 +141,7 @@ function generate_month_band() {
 		sanitized_date=$(date -d "${1}-${day}" "+%Y-%m-%d") 2>/dev/null || continue
 		[[ $today == $sanitized_date ]] && break
 		daylight_filepath="$PIC_DIR/$sanitized_date/daylight.png"
+		# If there is no daylight data, try to generate it
 		[[ -f $daylight_filepath ]] || generate_day_band $PIC_DIR/$sanitized_date
 		if test -f $daylight_filepath ; then
 			filelist=$(echo -n "$filelist $daylight_filepath")
@@ -220,37 +154,6 @@ function generate_month_band() {
 	convert +append $(echo $filelist) -colors 256 PNG8:$result_file
 }	
 
-function generate_year_band() {
-	# args: year-month
-	
-	#Checks
-	DAYLIGHT_DIR="$PIC_DIR/daylight"
-	[[ -d $DAYLIGHT_DIR ]] || mkdir $DAYLIGHT_DIR
-	empty_day_file="$DAYLIGHT_DIR/black.png"
-	[[ -f $empty_day_file ]] || generate_black_band $empty_day_file
-	result_file="$DAYLIGHT_DIR/$1.png"
-
-	today=$(date "+%Y-%m-%d")
-	filelist=""
-	for month in $(seq 1 12) ; do
-		for day in $(seq 1 31) ; do
-			#Exclude the days that don't exist and add zeros prefixes if needed
-			sanitized_date=$(date -d "${1}-${month}-${day}" "+%Y-%m-%d" 2>/dev/null) || continue
-			[[ $today == $sanitized_date ]] && break
-			daylight_filepath="$PIC_DIR/$sanitized_date/daylight.png"
-			[[ -f $daylight_filepath ]] || generate_day_band $PIC_DIR/$sanitized_date
-			if test -f $daylight_filepath ; then
-				filelist=$(echo -n "$filelist $daylight_filepath")
-			else
-				filelist=$(echo -n "$filelist $empty_day_file")
-			fi
-		done
-		[[ $today == $sanitized_date ]] && break
-	done
-
-	# actually create the png
-	convert +append $(echo $filelist) PNG8:$result_file
-}	
 
 function generate_months_from_to() {
 	from_year=$(echo $1 | cut -d '-' -f1)
@@ -269,60 +172,75 @@ function generate_months_from_to() {
 		done
 	done
 
-	echo "INFO Will generate daylight monthly bands for:$month_list"
-
 	for m in $month_list ; do
 		echo "INFO Generating month: $m"
-		generate_month_band $m
+		[[ html_only == 1 ]] || generate_month_band $m
+		generate_month_html $m
 	done
 }
 
-function generate_html() {
+
+########################
+# HTML GENERATION PART
+
+function dump_html_header() {
+	# Takes title of the page as an argument
+	echo "<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=0.8, user-scalable=no\">
+    <title>$title</title>
+    <link rel=\"stylesheet\" href=\"daylight.css\">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src=\"https://www.googletagmanager.com/gtag/js?id=G-TVGKVGZXNQ\"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-TVGKVGZXNQ');
+    </script>
+  </head>"
+}
+
+
+function generate_html_browser() {
 	#Build the list of all daylight monthly files
 	#Stitch them together and slap links on them
 	daylight_monthly_bands=$(find $PIC_DIR/daylight -type f -regextype sed -regex ".*/[0-9]\{4\}-[0-9]\{2\}\.png" -exec basename \{\} \;|sort -rn) 
 
 	# Generate the html
-	HTML_DIR=$(dirname $(readlink -f $0))/html
 	HTML_FILE=$TMP_DIR/daylight2.html
-	echo '<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no">
-    <title>isitfoggy daylight browser</title>
-    <link rel="stylesheet" href="daylight.css">
-  </head>
-    <body>' > $HTML_FILE
-	echo '
-      <div class="right">
-        <div class="timeboxeven">12 AM</div>
-        <div class="timeboxodd">1 AM</div>
-        <div class="timeboxeven">2 AM</div>
-        <div class="timeboxodd">3 AM</div>
-        <div class="timeboxeven">4 AM</div>
-        <div class="timeboxodd">5 AM</div>
-        <div class="timeboxeven">6 AM</div>
-        <div class="timeboxodd">7 AM</div>
-        <div class="timeboxeven">8 AM</div>
-        <div class="timeboxodd">9 AM</div>
-        <div class="timeboxeven">10 AM</div>
-        <div class="timeboxodd">11 AM</div>
-        <div class="timeboxeven">12 PM</div>
-        <div class="timeboxodd">1 PM</div>
-        <div class="timeboxeven">2 PM</div>
-        <div class="timeboxodd">3 PM</div>
-        <div class="timeboxeven">4 PM</div>
-        <div class="timeboxodd">5 PM</div>
-        <div class="timeboxeven">6 PM</div>
-        <div class="timeboxodd">7 PM</div>
-        <div class="timeboxeven">8 PM</div>
-        <div class="timeboxodd">9 PM</div>
-        <div class="timeboxeven">10 PM</div>
-        <div class="timeboxodd">11 PM</div>
-      </div>' >> $HTML_FILE
-	echo '
-      <div class="bands">' >> $HTML_FILE
+	dump_html_header "Daylight browser: isitfoggy" > $HTML_FILE
+	echo '  <body>
+    <div class="right">
+      <div class="timeboxeven">12 AM</div>
+      <div class="timeboxodd">1 AM</div>
+      <div class="timeboxeven">2 AM</div>
+      <div class="timeboxodd">3 AM</div>
+      <div class="timeboxeven">4 AM</div>
+      <div class="timeboxodd">5 AM</div>
+      <div class="timeboxeven">6 AM</div>
+      <div class="timeboxodd">7 AM</div>
+      <div class="timeboxeven">8 AM</div>
+      <div class="timeboxodd">9 AM</div>
+      <div class="timeboxeven">10 AM</div>
+      <div class="timeboxodd">11 AM</div>
+      <div class="timeboxeven">12 PM</div>
+      <div class="timeboxodd">1 PM</div>
+      <div class="timeboxeven">2 PM</div>
+      <div class="timeboxodd">3 PM</div>
+      <div class="timeboxeven">4 PM</div>
+      <div class="timeboxodd">5 PM</div>
+      <div class="timeboxeven">6 PM</div>
+      <div class="timeboxodd">7 PM</div>
+      <div class="timeboxeven">8 PM</div>
+      <div class="timeboxodd">9 PM</div>
+      <div class="timeboxeven">10 PM</div>
+      <div class="timeboxodd">11 PM</div>
+    </div>' >> $HTML_FILE
+	echo '    <div class="bands">' >> $HTML_FILE
 	for month_band in $daylight_monthly_bands; do 
 		width=$(( 1 * $(identify -format "%w"  $PIC_DIR/daylight/$month_band)))
 		month_name=$(echo $month_band | cut -d "." -f1)
@@ -335,8 +253,7 @@ function generate_html() {
             </a>
           </div>
           <div class=\"month\"><p>$month_pretty_name</p></div>
-        </div>
-" >> $HTML_FILE
+        </div>" >> $HTML_FILE
 	done
 	echo '
       </div>
@@ -344,32 +261,19 @@ function generate_html() {
 </html>' >> $HTML_FILE
 	mv $HTML_FILE $HTML_DIR/
 
-## Now generate the individual month html files		
-#	for month_band in $daylight_monthly_bands; do 
-#		width=$(( 1 * $(identify -format "%w"  $PIC_DIR/daylight/$month_band)))
-#		month_name=$(echo $month_band | cut -d "." -f1)
-#		generate_monthly_html $month_name
-#	done
 	exit 0
 }
 
-function generate_monthly_html () {
+function generate_month_html () {
 	month=$1
 	width=$(( 1 * $(identify -format "%w"  $PIC_DIR/daylight/$month.png)))
 	month_pretty_name=$(date -d "$month-01" "+%b %Y")
 	month_band_web_path="daylight/$month.png"
-	HTML_DIR="$(dirname $(readlink -f $0))/html"
 	HTML_FILE="$TMP_DIR/daylight_$month.html"
-	echo '<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>isitfoggy daylight browser</title>
-    <link rel="stylesheet" href="daylight.css">
-  </head>
-    <body>' > $HTML_FILE
-	echo "<img src=\"photos/$month_band_web_path\" usemap=\"#daylight\" width=\"${width}\" height=\"1440\">">> $HTML_FILE
-	echo "<map name=\"daylight\">" >> $HTML_FILE
+	dump_html_header "$month browser: isitfoggy" > $HTML_FILE
+	echo "  <body>
+    <img src=\"photos/$month_band_web_path\" usemap=\"#daylight\" width=\"${width}\" height=\"1440\">
+    <map name=\"daylight\">" >> $HTML_FILE
 	x=0
 	for day in $(seq 1 $width) ; do
 		[[ $day -lt 10 ]] && day=$(echo "0$day")
@@ -379,61 +283,60 @@ function generate_monthly_html () {
                 <area shape=\"rect\" coords=\"$x,0,$((x+1)),1439\" alt=\"$isodate\" href=\"$dirlink\">" >> $HTML_FILE
 		x="$((x+1))"
 	done
-	echo '</map>     
-      </div>
-  <script src="lib/jquery.min.js"></script>
-  <script src="lib/jquery.rwdImageMaps.min.js"></script>
-
-    <script>
-    $(document).ready(function(e) { $("img[usemap]").rwdImageMaps();});
-    </script>
-    </body>
+	echo '    </map>     
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/jquery.rwdImageMaps.min.js"></script>
+    <script>$(document).ready(function(e) { $("img[usemap]").rwdImageMaps();});</script>
+  </body>
 </html>' >> $HTML_FILE
 	mv $HTML_FILE $HTML_DIR/
 		
 }
 
-#main
+
+
+
+#######
+# main
+
 set +u
-if [[ ! -z $year ]] ; then
-	set -u
-	generate_year_band $year
+
+if [[ $browser_only == 1 ]] ; then
+	generate_html_browser
 	exit 0
 fi
 
-if [[ $html_only == 1 ]] ; then
+if [[ ! -z $range ]] ; then
 	set -u	
-	generate_html
-	exit 0
-fi
-
-if [[ ! -z $month_from ]] && [[ ! -z $month_to ]] ; then
-	set -u	
+	month_from=$(echo "$range" | cut -d ',' -f1)
+	validate_iso_month "$month_from" || fatal "Bad iso month from: $month_from"
+	month_to=$(echo "$range" | cut -d ',' -f2)
+	validate_iso_month "$month_to" || fatal "Bad iso month to: $month_to"
 	generate_months_from_to $month_from $month_to
+	generate_html_browser
 	exit 0
 fi
 
 if [[ ! -z $month ]] ; then
 	set -u
-	generate_month_band $month
+	validate_iso_month "$month" || fatal "Bad iso month to: $month"
+	[[ html_only == 1 ]] || generate_month_band $month
+	generate_month_html $month
+	generate_html_browser
 	exit 0
 fi
 
-if [[ ! -z $numdays ]] ; then
-	set -u
-	generate_lastndayview $numdays
-	exit 0
-fi
 if [[ ! -z $day ]] ; then
-	working_dir="$PIC_DIR/$day"
+	sanitized_day=$(sanitize_iso_date "$day")
+	working_dir="$PIC_DIR/$sanitized_day"
+	month=$(echo $sanitized_day |cut -d '-' -f1-2)
 	if [[ $force == 1 ]] || ! test -f $working_dir/daylight.png ; then
 		set -u
 		generate_day_band $working_dir
 		if [[ $? == 0 ]] ; then
-			month=$(echo $day |cut -d '-' -f1-2)
 			generate_month_band $month
-			generate_monthly_html $month
-			generate_html
+			generate_month_html $month
+			generate_html_browser
 		else
 			exit 1
 		fi
@@ -441,7 +344,3 @@ if [[ ! -z $day ]] ; then
 		echo "WARN $working_dir/daylight.png already exists. use -f to overwrite"
 	fi
 fi
-
-#default
-#set +e
-#generate_yearview
